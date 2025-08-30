18da4fcd8a018b3681b58086b46d57c4b0986fe6
lozukka <lotsulotsu@gmail.com>
2025-08-30 19:43:18 +0300

    beginning of the js logic

--- project 2/script.js
+++ project 2/script.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+//variables, submit button
+let calculateBtn = document.getElementById("calculate");
+
+//do the math, eventlistener, function
+//remember not to divide by 0
+calculateBtn.addEventListener("click", (event) => {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  let operator = document.getElementById("operation").value;
+  let result = 0;
+  //show the result
+  if (operator === "add") {
+    result = 20;
+  } else {
+    result = 50;
+  }
+  document.getElementById("result").innerHTML = result;
+});
