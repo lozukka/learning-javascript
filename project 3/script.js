0692b7bbc8f54380636201ba4cc1987ebff693af
lozukka <lotsulotsu@gmail.com>
2025-08-31 16:53:26 +0300

    script to counting clicks

--- project 3/script.js
+++ project 3/script.js
@@ -1,1 +1,10 @@
-console.log("Hello from script.js!");
+let click = document.getElementById("clickMe");
+
+//eventlistener and function to add click count
+click.addEventListener("click", (event) => {
+  let count = Number(document.getElementById("displayCount").innerHTML);
+  //add 1 to count
+  count++;
+  //show the count
+  document.getElementById("displayCount").innerHTML = count;
+});
