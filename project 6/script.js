827e33a2e575b6218617148f13c1ab8960f147ca
lozukka <lotsulotsu@gmail.com>
2025-09-01 15:45:37 +0300

    Update script.js
    main logic done

--- project 6/script.js
+++ project 6/script.js
@@ -24,16 +24,23 @@ multiplyBtn.addEventListener("click", (event) => {
 // 1. Lue käyttäjän syöte
 function getUserInput() {
   // palauttaa syötetyn luvun Number-muodossa
+  return Number(document.getElementById("userInput").value);
 }
 
 // 2. Tarkista, että syöte on 0–10
-function validateInput(value) {
+function validateInput(input) {
   // palauttaa true tai false
+  if(input <= 10 && input >=0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // 3. Tyhjennä taulukko ennen uuden rakentamista
 function clearTable() {
   // poistaa aiemmat rivit taulukosta
+  resultTable.innerHTML = "";
 }
 
 // 4. Rakenna koko kertotaulukko
@@ -45,6 +52,15 @@ function buildTable(n) {
 // 5. Luo taulukon otsikkorivi
 function buildHeaderRow(n) {
   // tekee yhden <tr>-rivin, jossa <th>-soluja 0...n
+  const headerRow = document.createElement("tr");
+  let headerCount = 0;
+  for(let i = 0; i<=n; i++){
+    const headerTableData = document.createElement("th");
+    headerTableData.textContent = headerCount;
+    headerRow.appendChild(headerTableData);
+    headerCount++;
+  }
+  resultTable.appendChild(headerRow);
 }
 
 // 6. Luo taulukon sisältörivit (esim. kertolaskut)
@@ -52,9 +68,18 @@ function buildContentRows(n) {
   // jokaiselle riville <tr>
   // ensimmäinen solu = kerroin
   // seuraavat solut = kertolaskujen tulokset
+  const tableRow = document.createElement("tr");
+
+  for(let i=0; i<=n; i++){
+    const tableData = document.createElement("td");
+    tableData.textContent = i * n; 
+    tableRow.appendChild(tableData);
+  }
+  resultTable.appendChild(tableRow);
 }
 
 // 7. Näytä varoitusviesti
 function showWarning(message) {
   // laittaa tekstin warning-elementtiin
+  warning.textContent = message;
 }
