0a43ce1c1dc75e8f5ec18800f20cc748ebeb7a36
lozukka <lotsulotsu@gmail.com>
2025-09-02 12:41:55 +0300

    color switcher

--- project 9/script.js
+++ project 9/script.js
@@ -1,1 +1,15 @@
-console.log("hello")
+let randomColor = document.getElementById("change");
+let changeClassName = document.getElementById("changeclass");
+const backgroundColor = document.body;
+
+randomColor.addEventListener("click", (event)=>{
+    backgroundColor.style.background = createColor();
+})
+
+function createColor(){
+    return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
+}
+changeClassName.addEventListener("click", (e)=>{
+    backgroundColor.classList.toggle("dark");
+    backgroundColor.style = "";
+})
