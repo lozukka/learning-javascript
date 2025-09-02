f26e5e470ac38c19c6cb8862c325957f4cb04c3b
lozukka <lotsulotsu@gmail.com>
2025-09-02 15:54:39 +0300

    main logic done

--- project 11/script.js
+++ project 11/script.js
@@ -1,1 +1,30 @@
-console.log("hellooo")
+const startTimer = document.getElementById("start");
+const stopTimer = document.getElementById("stop");
+const timerDisplay = document.getElementById("timer");
+
+let timerId;
+let newTime = 10;
+
+function myTimer() {
+    timerDisplay.textContent = newTime;
+    newTime--;
+
+    if (newTime < 0) {
+        clearInterval(timerId);
+        timerId = null;
+    }
+}
+
+startTimer.addEventListener("click", () => {
+    if (!timerId) { // käynnistä vain jos ei jo käynnissä
+        if (newTime <= 0) {
+            newTime = 10; // resetoi alusta jos haluat uuden startin
+        }
+        timerId = setInterval(myTimer, 1000);
+    }
+});
+
+stopTimer.addEventListener("click", () => {
+    clearInterval(timerId);
+    timerId = null;
+});
