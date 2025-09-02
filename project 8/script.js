e6faf7ee8e42c690fe46a200989d89601a5dcbe5
lozukka <lotsulotsu@gmail.com>
2025-09-02 12:12:28 +0300

    Update script.js
    fixed and finnished script

--- project 8/script.js
+++ project 8/script.js
@@ -2,7 +2,8 @@
 //    - tekstialue, johon käyttäjä kirjoittaa
 //    - elementti, johon merkkimäärä näytetään
 let userInput = document.getElementById("userinput");
-let count = document.getElementById("count");
+let counter = document.getElementById("count");
+let clear = document.getElementById("clear");
 
 // 2. Lisää tapahtumakuuntelija tekstialueelle
 //    - käytä "input"-tapahtumaa, jotta reagointi on reaaliaikaista
@@ -10,13 +11,26 @@ userInput.addEventListener("input", (event) =>{
 // 3. Tapahtumankäsittelijän sisällä:
 //    - hae käyttäjän kirjoittaman tekstin pituus
 //    - päivitä laskurin arvo näytölle
-const length = userInput.value.length;
-count.textContent = length;
+    const length = userInput.value.length;
+    checkLength(length);
+    counter.textContent = length;
+});
+
+// 4. (Valinnainen) Lisää lisälogiikkaa
+//    - vaihda laskurin väri punaiseksi, jos merkkimäärä ylittää rajan
+function checkLength(length){
+    if(length>20){
+        counter.style.color= "red";
+    }else{
+        counter.style.color= "black";
+    }
+}
+
+clear.addEventListener("click", () => {
+    userInput.value = "";        // tyhjentää tekstialueen
+    counter.textContent = "0";     // nollaa laskurin
+    counter.style.color = "black"; // palauttaa värin
 });
 
 
 
-// 4. (Valinnainen) Lisää lisälogiikkaa
-//    - vaihda laskurin väri punaiseksi, jos merkkimäärä ylittää rajan
-//    - näytä myös sanamäärä
-//    - aseta maksimi­merkki­määrä ja estä kirjoittaminen sen jälkeen
