6370de341ad0aa85902939169cd0c33afa90c60c
lozukka <lotsulotsu@gmail.com>
2025-09-03 10:56:17 +0300

    working with the logic

--- project 12/script.js
+++ project 12/script.js
@@ -3,17 +3,31 @@ let resultText = document.getElementById("result");
 
 function checkAnswer() {
   
-  const selected = document.querySelector('input[name="q1"]:checked');
-
+  const selected = [document.querySelector('input[name="q1"]:checked').value, document.querySelector('input[name="q2"]:checked').value, document.querySelector('input[name="q3"]:checked').value] ;
+const rightAnswers = ["4", "7", "8"]
   if (selected) {
-    console.log("Valitsit:", selected.value);
-    
-    if(selected.value === "4"){
-        resultText.textContent = "You chose right!"
-    } else {
-        resultText.textContent = "You chose wrong!"
-    }
+    console.log("Valitsit:", selected);
+    let score = 0;
+
+    checkAnswers(selected, rightAnswers);
+    showResult(score);
+
   } else {
     console.log("Et valinnut mitään.");
   }
+}
+
+function checkAnswers(selected, rightAnswers) {
+  let score = 0;
+  selected.forEach((element, index) => {
+    if (element === rightAnswers[index]) {
+      score++;
+    }
+  });
+  return score;
+}
+
+function showResult(score){
+    resultText.textContent = `Your score today is: ${score}!`
+
 }
