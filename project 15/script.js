c72399d682ccb4582cf117c4065355365ae16e34
lozukka <lotsulotsu@gmail.com>
2025-09-03 20:05:10 +0300

    Update script.js
    fixed the local storage

--- project 15/script.js
+++ project 15/script.js
@@ -8,8 +8,16 @@ function addItem(name, price) {
   cart.push({ name, price });
 
   addToCart(name);
+  saveCartItem(name, price);
   addTotal(price);
   console.log(cart);
+}
+
+function saveCartItem(name, price) {
+  const saved = localStorage.getItem("cartItems");
+  const cartItems = saved ? JSON.parse(saved) : [];
+  cartItems.push({ name, price });
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
 }
 
 function addToCart(n) {
@@ -22,3 +30,17 @@ function addTotal(p) {
   sum = cart.reduce((total, item) => total + item.price, 0);
   sumDisplay.textContent = `Total: ${sum}€`;
 }
+
+function loadCart() {
+  const saved = localStorage.getItem("cartItems");
+  if (saved) {
+    const tasks = JSON.parse(saved);
+    tasks.forEach((cartItem) => {
+      cart.push(cartItem); // lisää takaisin cartiin
+      addToCart(cartItem.name); // näytä listassa
+    });
+    addTotal(0); // laskee koko summan uudestaan cartista
+  }
+}
+
+window.onload = loadCart;
