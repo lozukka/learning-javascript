04dc6bc166dacb43e0554bf02412e384afc31b2f
lozukka <lotsulotsu@gmail.com>
2025-09-03 09:47:40 +0300

    redesigned form

--- project 12/script.js
+++ project 12/script.js
@@ -1,1 +1,19 @@
 console.log("hellooo")
+let resultText = document.getElementById("result");
+
+function checkAnswer() {
+  
+  const selected = document.querySelector('input[name="q1"]:checked');
+
+  if (selected) {
+    console.log("Valitsit:", selected.value);
+    
+    if(selected.value === "4"){
+        resultText.textContent = "You chose right!"
+    } else {
+        resultText.textContent = "You chose wrong!"
+    }
+  } else {
+    console.log("Et valinnut mitään.");
+  }
+}
