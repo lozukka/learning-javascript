64ac1607a16d2ff5891423451e45624f2372f1fe
lozukka <lotsulotsu@gmail.com>
2025-09-03 15:50:43 +0300

    beginning of the logic

--- project 15/script.js
+++ project 15/script.js
@@ -1,1 +1,24 @@
-console.log("helloo")
+let itemList = document.getElementById("item-list")
+let sumDisplay = document.getElementById("sum-text")
+
+let cart = [];
+//cart.push({ name: "Apple", price: 2 });
+
+//each button contains a name and a price
+
+//eventlistener
+function addApple(){
+    cart.push({name: "Apple", price: 1});
+    //function to add price
+    //function to add list item
+    addToCart("Apple");
+    console.log(cart);
+}
+//main function, triggered by add-button
+//add item to cart, use push method
+//show item in cart
+function addToCart(n){
+    itemList.textContent = n;
+}
+//add price to total, use reduce method
+//show new total
