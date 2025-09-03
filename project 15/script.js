b30e3e9d8db9a7b77c8de0e025be90c4c724da4f
lozukka <lotsulotsu@gmail.com>
2025-09-03 19:19:20 +0300

    main basic logic

--- project 15/script.js
+++ project 15/script.js
@@ -1,24 +1,54 @@
-let itemList = document.getElementById("item-list")
-let sumDisplay = document.getElementById("sum-text")
+let itemList = document.getElementById("item-list");
+let sumDisplay = document.getElementById("sum-text");
 
 let cart = [];
+let sum = 0;
 //cart.push({ name: "Apple", price: 2 });
 
 //each button contains a name and a price
 
 //eventlistener
-function addApple(){
-    cart.push({name: "Apple", price: 1});
-    //function to add price
-    //function to add list item
-    addToCart("Apple");
-    console.log(cart);
+function addApple() {
+  cart.push({ name: "Apple", price: 1 });
+  //function to add price
+  //function to add list item
+  addToCart("Apple");
+  addTotal(1);
+  console.log(cart);
+}
+function addJuice() {
+  cart.push({ name: "Juice", price: 2 });
+  //function to add price
+  //function to add list item
+  addToCart("Juice");
+  addTotal(2);
+  console.log(cart);
+}
+function addBread() {
+  cart.push({ name: "Bread", price: 4 });
+  //function to add price
+  //function to add list item
+  addToCart("Bread");
+  addTotal(4);
+  console.log(cart);
+}
+function addChicken() {
+  cart.push({ name: "Chicken", price: 6 });
+  //function to add price
+  //function to add list item
+  addToCart("Chicken");
+  addTotal(6);
+  console.log(cart);
 }
 //main function, triggered by add-button
 //add item to cart, use push method
 //show item in cart
-function addToCart(n){
-    itemList.textContent = n;
+function addToCart(n) {
+  itemList.textContent = n;
 }
 //add price to total, use reduce method
+function addTotal(p) {
+  sum = sum + p;
+  sumDisplay.textContent = sum;
+}
 //show new total
